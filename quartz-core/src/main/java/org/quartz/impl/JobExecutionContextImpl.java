ec91ab324c0807b4bf037402caba923a1cfbd9f3
kyle <li_yu@worksap.co.jp>
2017-10-19 17:22:15 +0800

    refs #192 job execution context impl returns correct recovering job key

--- quartz-core/src/main/java/org/quartz/impl/JobExecutionContextImpl.java
+++ quartz-core/src/main/java/org/quartz/impl/JobExecutionContextImpl.java
@@ -144,8 +144,8 @@ public class JobExecutionContextImpl implements java.io.Serializable, JobExecuti
 
     public TriggerKey getRecoveringTriggerKey() {
         if (isRecovering()) {
-            return new TriggerKey(jobDataMap.getString(Scheduler.FAILED_JOB_ORIGINAL_TRIGGER_GROUP),
-                                  jobDataMap.getString(Scheduler.FAILED_JOB_ORIGINAL_TRIGGER_NAME));
+            return new TriggerKey(jobDataMap.getString(Scheduler.FAILED_JOB_ORIGINAL_TRIGGER_NAME),
+                                  jobDataMap.getString(Scheduler.FAILED_JOB_ORIGINAL_TRIGGER_GROUP));
         } else {
             throw new IllegalStateException("Not a recovering job");
         }
